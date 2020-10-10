cd099649677afde08e1ad47218ee5e06b2f12fa9
Carlos Roberto <carlosroberto15755@gmail.com>
2020-10-10 01:37:44 -0300

    Momento ate instalacao do angular-route

---
+++ app/acesso/acesso.controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+  
+  angular.module('listaComprasApp')
+    .controller('AcessoController', acessoController)
+
+  acessoController.$inject = []
+
+  function acessoController() {
+    var vm = this
+
+  }
+
+})()
---
+++ app/header.controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+  
+  angular.module('listaComprasApp')
+    .controller('HeaderController', headerController)
+
+  headerController.$inject = []
+
+  function headerController() {
+    var vm = this
+
+  }
+
+})()
